06272ed5ea78075498d1fb1a7ec70399dc5c605c
Adalberto Maiam <btowmaia@gmail.com>
2021-03-13 22:08:33 -0300

    reformulando

---
+++ backend/src/main/java/com/aminterprise/amdeliver/AmdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.aminterprise.amdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aminterprise/amdeliver/AmdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aminterprise.amdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
