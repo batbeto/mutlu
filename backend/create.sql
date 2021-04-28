19b7ded3d6db055bf13c05d7822f308717268389
Adalberto Maiam <btowmaia@gmail.com>
2021-04-28 13:56:39 -0300

    front prod

--- backend/create.sql
+++ backend/create.sql
@@ -1,5 +1,10 @@
-create table tb_event (id  bigserial not null, address varchar(255), date timestamp, description varchar(255), image_uri varchar(255), latitude float8, longitude float8, name varchar(255), price float8, tickets int8, primary key (id));
-create table tb_order (id  bigserial not null, moment timestamp, price float8, qtd int4, status int4, total float8, primary key (id));
+create table tb_event (id  bigserial not null, address varchar(255), date timestamp, description varchar(255), image_uri varchar(255), latitude float8, longitude float8, name varchar(255), price float8, status int4, tickets int8, primary key (id));
+create table tb_order (id  bigserial not null, moment timestamp, qtd int4, status int4, total float8, user_id int8, primary key (id));
 create table tb_order_event (order_id int8 not null, event_id int8 not null, primary key (order_id, event_id));
+create table tb_user (id  bigserial not null, cpf varchar(255), email varchar(255), name varchar(255), pass varchar(255), status int4, primary key (id));
+create table tb_user_history (user_id int8 not null, event_id int8 not null, primary key (user_id, event_id));
+alter table if exists tb_user_history add constraint UK_cd1rmd2argx0xrmgp7be8rrvl unique (event_id);
 alter table if exists tb_order_event add constraint FK275vl57ibx7yw5d3rr0ym71hm foreign key (event_id) references tb_event;
 alter table if exists tb_order_event add constraint FK67uejshkqrcaqn67gjqmqmm foreign key (order_id) references tb_order;
+alter table if exists tb_user_history add constraint FKtp2po8bxsfljnyt51rkug5hwj foreign key (event_id) references tb_event;
+alter table if exists tb_user_history add constraint FKrde89thx2r5wyw87hk2fqud5n foreign key (user_id) references tb_user;
--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -5,14 +5,15 @@ import { User } from "./services/typesUser";
 
 
 const local = 'http://localhost:8080';
+const API_URL = process.env.REACT_APP_API_URL;
 
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 //Events
-export const fetchEvents = () => ( axios(`${local}/events/all`));
-export const fetchAllEvents = () => (axios(`${local}/events/all`));
-export const postEvents = (data: Event) => (axios.post(`${local}/events`, data));
-export const putEvents = (data: Event) => (axios.put(`${local}/events/${data.id}`, data))
+export const fetchEvents = () => ( axios(`${API_URL}/events/all`));
+export const fetchAllEvents = () => (axios(`${API_URL}/events/all`));
+export const postEvents = (data: Event) => (axios.post(`${API_URL}/events`, data));
+export const putEvents = (data: Event) => (axios.put(`${API_URL}/events/${data.id}`, data))
 
 //Map
 export const fetchLocalMapBox = (local: string) =>
@@ -20,12 +21,12 @@ export const fetchLocalMapBox = (local: string) =>
 
 
 //Orders
-export const fetchOrder = () =>(axios(`${local}/orders`));
-export const postOrders = (data: OrderPayload) => (axios.post(`${local}/orders`, data));
+export const fetchOrder = () =>(axios(`${API_URL}/orders`));
+export const postOrders = (data: OrderPayload) => (axios.post(`${API_URL}/orders`, data));
 
 //Users
-export const fetchUsers = () => (axios(`${local}/users`));
-export const loginUser = (user: {email: string, pass: string}) => (axios.post(`${local}/users/login`),user)
-export const postUsers = (data: User) => (axios.post(`${local}/users`, data))
-export const putUsers = (data: User) => (axios.put(`${local}/users/${data.id}`, data))
+export const fetchUsers = () => (axios(`${API_URL}/users`));
+export const loginUser = (user: {email: string, pass: string}) => (axios.post(`${API_URL}/users/login`),user)
+export const postUsers = (data: User) => (axios.post(`${API_URL}/users`, data))
+export const putUsers = (data: User) => (axios.put(`${API_URL}/users/${data.id}`, data))
 
